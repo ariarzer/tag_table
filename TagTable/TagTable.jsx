39bdd8c88d5776f8d714aa42931661616c46a210
Sofi Valitova <ariarzer@gmail.com>
2021-04-13 17:23:06 +0300

    render only select rows with selectedTags props

--- TagTable/TagTable.jsx
+++ TagTable/TagTable.jsx
@@ -12,7 +12,20 @@ export function TagTable(props) {
   return <table className={'TagTable'}>
     <Thead fields={collectDataFields(data)}/>
     <tbody>
-    {Object.keys(data).map((colorName) => <Tr key={colorName} colorName={colorName} fields={data[colorName]}/>)}
+    {Object.keys(data).map((colorName) => {
+      if (isRowContainAllTags(data[colorName], props.selectedTags)) {
+        return <Tr key={colorName} colorName={colorName} fields={data[colorName]}/>
+      }
+    })}
     </tbody>
   </table>;
+}
+
+function isRowContainAllTags(row, tags) {
+  if (!row.tags || !tags) {
+    return true;
+  }
+  return tags.reduce((acc, cur) => {
+    return acc & row.tags.includes(cur);
+  }, true)
 }
